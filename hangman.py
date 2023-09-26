6338535c3fe25fd4a59cffb1793bd5d0df124345
kent.mercado <merken.work@gmail.com>
2023-09-27 00:19:42 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,35 +3,41 @@ print("Welcome To HANGMAN GAME")
 def searchWord(lett):
     for i in arrGuess:
         if i == lett:
+            dictGuess.update({i:True})
+            displayClue()
             arrGuess.remove(i)
             return True
 
-def displayClue(no):
-    if (no == 0):
-        print("\nThe Game will now START!!!\n\n")
-        for i in arrGuess:
+def displayClue():
+    print("-----"*15)
+    print("\n\n")
+    for i in dictGuess:
+        if(dictGuess.get(i) == True):
+            print(i, end =" ")
+        else:
             print("__  ", end ="" )
-        print("\n\n\n")
-        return
     
-        
-
+    print("\n\n") #spacer
+      
+    
 arrGuess=[]
+dictGuess={}
 guess = input("Enter a word to guess: ")
 
 for i in guess:
-    arrGuess.append(i)
-##    print(i, end= ", ")
+    arrGuess.append(i.upper())
+    dictGuess[i.upper()] = False
 
-##print(arrGuess) ##@@
-##arrGuess.sort()
-displayClue(0)
+##print(arrGuess,dictGuess) ##@@ checker
+print("\nThe Game will now START!!!\n\n")
+displayClue()
 
 word = False
 while(guess != word):
     oneLet =input("Enter a single letter: ")
+    oneLet = oneLet.upper()
     if (searchWord(oneLet) == True):
-        print(oneLet + " is Correct")
-        print(arrGuess) # checker
+        print("\n",oneLet , " is Correct")        
+##        print(arrGuess) ##@@  checker
     else:
         print("incorrect")
