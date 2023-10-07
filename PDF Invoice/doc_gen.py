31411fa0ab679a763d6589dd7802ef40df905d4b
kent.mercado <merken.work@gmail.com>
2023-10-07 15:21:36 +0800

    finish docs template

--- PDF Invoice/doc_gen.py
+++ PDF Invoice/doc_gen.py
@@ -2,13 +2,13 @@ from docxtpl import DocxTemplate
 
 doc = DocxTemplate("invoice_template.docx")
 
-invoice_list = [["OCT1","OCT4","PM","Cris","no guide",3,10000]]
+invoice_list = [["OCT1","OCT4","PM","Cris","no guide",3,10000],["OCT1","OCT4","PM","Caiden","with",3,10000],["OCT1","OCT4","PM","Kent","gidu",3,10000]]
 
 doc.render({
             "company":"Wow",
             "date_today":"date today",
             "invoice_list": invoice_list,
-            "grand_total": 20999
+            "grand_total": 30999
             
             })
 doc.save("new_invoice.docx")
